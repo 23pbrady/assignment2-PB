4f5ed5c619ed4a14c5d91269bdc019c6f4bf34ae
Patrick Brady <pbrady@wesleyan.edu>
2026-09-21 09:17:53 -0400

    Added a new graph

--- penguin_analysis.R
+++ penguin_analysis.R
@@ -1,3 +1,7 @@
+# NEXT 2 LINES FROM AI --  Place this at the very top of your script
+log_file <- file("script_output_log.txt", open = "wt")
+sink(log_file, type = "output", split = TRUE)
+
 setwd("~/Desktop/Fall 2026 Resources/QAC380/assignment2-PB/")
 penguins <- read.csv("penguins.csv")
 
@@ -8,3 +12,9 @@ freq(penguins$sex)
 
 my.lm <- lm(body_mass_g ~ bill_length_mm + flipper_length_mm + bill_depth_mm, data = penguins) 
 summary(my.lm)
+
+ggplot(data=penguins)+geom_point(aes(x=flipper_length_mm, y=body_mass_g))+geom_smooth(aes(x=flipper_length_mm, y=body_mass_g))
+
+# NEXT 2 LINES FROM AI -- Place this at the very bottom to cleanly close the log
+sink(type = "output")
+sink(type = "message")
